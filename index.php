77a1daee1d2213ac3a0d4af7ea701415bc9995d9
Haris ur Rehman <hrehman200@gmail.com>
2022-12-11 03:39:50 +0500

    Initial working version delivered to client

---
+++ index.php
@@ -0,0 +1,60 @@
+<?php
+require_once 'vendor/autoload.php';
+
+define('IMAP_HOST', 'xxx');
+define('IMAP_USERNAME', 'xxx');
+define('IMAP_PASS', 'xxx');
+define('LEXOFFICE_API_TOKEN', 'xxx');
+define('ATTACHMENT_FOLDER', __DIR__ . '/attachments/');
+
+$mailbox = new \Pb\Imap\Mailbox(
+    IMAP_HOST,
+    IMAP_USERNAME,
+    IMAP_PASS,
+    'INBOX',
+    __DIR__ . '/attachments',
+    [
+        \Pb\Imap\Mailbox::OPT_DEBUG_MODE => true
+    ]
+);
+$messageIds = $mailbox->search('UNSEEN');
+
+foreach ($messageIds as $messageId) {
+    $message = $mailbox->getMessage($messageId);
+
+    // print_r($message);
+    // print_r($message->getAttachments());
+
+    $attachments = $message->getAttachments();
+    foreach($attachments as $attachment) {
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://api.lexoffice.io./v1/files',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => array('file' => new CURLFILE(ATTACHMENT_FOLDER . $attachment->filepath), 'type' => 'voucher'),
+            CURLOPT_HTTPHEADER => array(
+                'Authorization: Bearer ' . LEXOFFICE_API_TOKEN,
+                'Accept: application/json'
+            ),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        echo $response;
+
+        $response = json_decode($response, true);
+        if(array_key_exists('id', $response)) {
+            unlink(ATTACHMENT_FOLDER . $attachment->filepath);
+        }
+    }    
+}
+
