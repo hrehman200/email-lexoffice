dcf70e37d42bbfcb7074c2543aacd8ac6c014e6f
Haris ur Rehman <hrehman200@gmail.com>
2023-01-21 14:44:41 +0500

    New logo + css resources serve from our server

--- application/views/layout.php
+++ application/views/layout.php
@@ -5,40 +5,61 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta content="width=device-width, height=device-height, initial-scale=1" name="viewport" />
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link rel="shortcut icon" href="<?= base_url() ?>resources/img/icons/icon-48x48.png" />
+    <!-- <link rel="preconnect" href="https://fonts.gstatic.com"> -->
+    <!-- <link rel="shortcut icon" href="<?= base_url() ?>resources/img/icons/icon-48x48.png" /> -->
 
     <title>Email Invoice</title>
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?= base_url() ?>resources/css/bootstrap.min.css" rel="stylesheet">
     <link href="<?= base_url() ?>resources/css/app.css" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap" rel="stylesheet">
+    <link href="<?= base_url() ?>resources/fonts/fonts.css" rel="stylesheet">
+
+    <meta name="msapplication-TileImage"
+        content="https://email-invoice.de/wp-content/uploads/cropped-EMAIL-INVOICE-Icon-512-270x270.png">
+    <link rel="icon" href="https://email-invoice.de/wp-content/uploads/cropped-EMAIL-INVOICE-Icon-512-32x32.png"
+        sizes="32x32">
+    <link rel="icon" href="https://email-invoice.de/wp-content/uploads/cropped-EMAIL-INVOICE-Icon-512-192x192.png"
+        sizes="192x192">
+    <link rel="apple-touch-icon"
+        href="https://email-invoice.de/wp-content/uploads/cropped-EMAIL-INVOICE-Icon-512-180x180.png">
 
     <style type="text/css">
-        .content{background-color: #ffffff!important;}
-        .btn-color{background-color: #FF0766!important; color: #ffffff!important;}
-        .signup-form input{border: 1px solid #B7B7CB;border-radius: 0px;}
-        .logo_anchor{
-                width: 363px;
+        .content {
+            background-color: #ffffff !important;
         }
-        .action_btn a span{
-            color: #FF0766!important;
-            font-weight: 800!important;
-            font-family: 'Arial',Helvetica,Arial,Lucida,sans-serif;
+
+        .btn-color {
+            background-color: #eb6f6c !important;
+            color: #ffffff !important;
         }
-        .header_nav{
+
+        .signup-form input {
+            border: 1px solid #B7B7CB;
+            border-radius: 0px;
+        }
+
+        .logo_anchor {
+            width: 363px;
+        }
+
+        .action_btn a span {
+            color: #eb6f6c !important;
+            font-weight: 800 !important;
+            font-family: 'Arial', Helvetica, Arial, Lucida, sans-serif;
+        }
+
+        .header_nav {
             padding-bottom: 20px;
             border: 1px solid lightgray;
         }
 
-        .logo_anchor img{
-                height: 40px; 
-                width: 202px!important; 
-                /* margin-left: 112px; */
+        .logo_anchor img {
+            height: 40px;
+            width: 202px !important;
+            /* margin-left: 112px; */
         }
 
-        @media screen and (max-width: 480px)
-        {
+        @media screen and (max-width: 480px) {
             .mb-5 {
                 margin-bottom: 200px !important;
             }
@@ -58,7 +79,8 @@
                         </div>
                         <div class="simplebar-mask">
                             <div class="simplebar-offset" style="right: 0px; bottom: 0px;">
-                                <div class="simplebar-content-wrapper" tabindex="0" role="region" aria-label="scrollable content" style="height: 100%; overflow: hidden scroll;">
+                                <div class="simplebar-content-wrapper" tabindex="0" role="region"
+                                    aria-label="scrollable content" style="height: 100%; overflow: hidden scroll;">
                                     <div class="simplebar-content" style="padding: 0px;">
                                         <a class="sidebar-brand" href="javascript:;">
                                             <span class="align-middle">Email Invoice</span>
@@ -70,8 +92,11 @@
                                             </li>
 
                                             <li class="sidebar-item active">
-                                                <a class="sidebar-link" href="<?=site_url('dashboard')?>">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-sliders align-middle">
+                                                <a class="sidebar-link" href="<?= site_url('dashboard') ?>">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                                        viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                                        stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                                        class="feather feather-sliders align-middle">
                                                         <line x1="4" y1="21" x2="4" y2="14"></line>
                                                         <line x1="4" y1="10" x2="4" y2="3"></line>
                                                         <line x1="12" y1="21" x2="12" y2="12"></line>
@@ -86,43 +111,43 @@
                                             </li>
 
                                             <!-- <li class="sidebar-item">
-                                            <a class="sidebar-link" href="pages-profile.html">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user align-middle">
-                                                    <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
-                                                    <circle cx="12" cy="7" r="4"></circle>
-                                                </svg> <span class="align-middle">Profile</span>
-                                            </a>
-                                        </li>
+                                                                    <a class="sidebar-link" href="pages-profile.html">
+                                                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user align-middle">
+                                                                            <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+                                                                            <circle cx="12" cy="7" r="4"></circle>
+                                                                        </svg> <span class="align-middle">Profile</span>
+                                                                    </a>
+                                                                </li>
 
-                                        <li class="sidebar-item">
-                                            <a class="sidebar-link" href="pages-sign-in.html">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-log-in align-middle">
-                                                    <path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4"></path>
-                                                    <polyline points="10 17 15 12 10 7"></polyline>
-                                                    <line x1="15" y1="12" x2="3" y2="12"></line>
-                                                </svg> <span class="align-middle">Sign In</span>
-                                            </a>
-                                        </li>
+                                                                <li class="sidebar-item">
+                                                                    <a class="sidebar-link" href="pages-sign-in.html">
+                                                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-log-in align-middle">
+                                                                            <path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4"></path>
+                                                                            <polyline points="10 17 15 12 10 7"></polyline>
+                                                                            <line x1="15" y1="12" x2="3" y2="12"></line>
+                                                                        </svg> <span class="align-middle">Sign In</span>
+                                                                    </a>
+                                                                </li>
 
-                                        <li class="sidebar-item">
-                                            <a class="sidebar-link" href="pages-sign-up.html">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user-plus align-middle">
-                                                    <path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
-                                                    <circle cx="8.5" cy="7" r="4"></circle>
-                                                    <line x1="20" y1="8" x2="20" y2="14"></line>
-                                                    <line x1="23" y1="11" x2="17" y2="11"></line>
-                                                </svg> <span class="align-middle">Sign Up</span>
-                                            </a>
-                                        </li>
+                                                                <li class="sidebar-item">
+                                                                    <a class="sidebar-link" href="pages-sign-up.html">
+                                                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user-plus align-middle">
+                                                                            <path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
+                                                                            <circle cx="8.5" cy="7" r="4"></circle>
+                                                                            <line x1="20" y1="8" x2="20" y2="14"></line>
+                                                                            <line x1="23" y1="11" x2="17" y2="11"></line>
+                                                                        </svg> <span class="align-middle">Sign Up</span>
+                                                                    </a>
+                                                                </li>
 
-                                        <li class="sidebar-item">
-                                            <a class="sidebar-link" href="pages-blank.html">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-book align-middle">
-                                                    <path d="M4 19.5A2.5 2.5 0 0 1 6.5 17H20"></path>
-                                                    <path d="M6.5 2H20v20H6.5A2.5 2.5 0 0 1 4 19.5v-15A2.5 2.5 0 0 1 6.5 2z"></path>
-                                                </svg> <span class="align-middle">Blank</span>
-                                            </a>
-                                        </li> -->
+                                                                <li class="sidebar-item">
+                                                                    <a class="sidebar-link" href="pages-blank.html">
+                                                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-book align-middle">
+                                                                            <path d="M4 19.5A2.5 2.5 0 0 1 6.5 17H20"></path>
+                                                                            <path d="M6.5 2H20v20H6.5A2.5 2.5 0 0 1 4 19.5v-15A2.5 2.5 0 0 1 6.5 2z"></path>
+                                                                        </svg> <span class="align-middle">Blank</span>
+                                                                    </a>
+                                                                </li> -->
 
 
                                         </ul>
@@ -141,44 +166,47 @@
 
                 <div class="col-10 offset-1">
 
-                <a class="sidebar-toggle js-sidebar-toggle logo_anchor float-start" href="http://email-invoice.de">
-                   <img src="<?=base_url('resources/img/logo.png')?>">
-                </a>
+                    <a class="sidebar-toggle js-sidebar-toggle logo_anchor float-start" href="http://email-invoice.de">
+                        <img src="<?= base_url('resources/img/logo.png') ?>">
+                    </a>
 
-                <?php if (isset($_SESSION['name'])) { ?>
-                    <div class="navbar-collapse collapse">
-                        <ul class="navbar-nav navbar-align">
-                            <li class="nav-item dropdown action_btn">
-                                <a class="nav-link dropdown-toggle d-none d-sm-inline-block" href="#" data-bs-toggle="dropdown">
-                                    <span class="text-dark"><?= $_SESSION['name'] ?></span>
-                                </a>
-                                <div class="dropdown-menu dropdown-menu-end">
-                                    <a class="dropdown-item" href="<?= site_url('auth/logout') ?>">Log out</a>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                <?php } else if ($this->uri->segment(2) == 'signin') { ?>
-                    <div class="navbar-collapse collapse">
-                        <ul class="navbar-nav navbar-align">
-                            <li class="nav-item dropdown action_btn">
-                                <a class="nav-link d-inline-block" href="<?= site_url('auth/create_user') ?>">
-                                    <span class="text-dark">Signup</span>
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                <?php } else { ?>
-                    <div class="navbar-collapse collapse float-right">
-                        <ul class="navbar-nav navbar-align">
-                            <li class="nav-item dropdown action_btn">
-                                <a class="nav-link d-inline-block" href="<?= site_url('auth/signin') ?>">
-                                    <span class="text-dark">Login</span>
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                <?php } ?>
+                    <?php if (isset($_SESSION['name'])) { ?>
+                        <div class="navbar-collapse collapse">
+                            <ul class="navbar-nav navbar-align">
+                                <li class="nav-item dropdown action_btn">
+                                    <a class="nav-link dropdown-toggle d-none d-sm-inline-block" href="#"
+                                        data-bs-toggle="dropdown">
+                                        <span class="text-dark">
+                                            <?= $_SESSION['name'] ?>
+                                        </span>
+                                    </a>
+                                    <div class="dropdown-menu dropdown-menu-end">
+                                        <a class="dropdown-item" href="<?= site_url('auth/logout') ?>">Log out</a>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    <?php } else if ($this->uri->segment(2) == 'signin') { ?>
+                            <div class="navbar-collapse collapse">
+                                <ul class="navbar-nav navbar-align">
+                                    <li class="nav-item dropdown action_btn">
+                                        <a class="nav-link d-inline-block" href="<?= site_url('auth/create_user') ?>">
+                                            <span class="text-dark">Signup</span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                    <?php } else { ?>
+                            <div class="navbar-collapse collapse float-right">
+                                <ul class="navbar-nav navbar-align">
+                                    <li class="nav-item dropdown action_btn">
+                                        <a class="nav-link d-inline-block" href="<?= site_url('auth/signin') ?>">
+                                            <span class="text-dark">Login</span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                    <?php } ?>
 
                 </div>
 
